5f75c14c7cd12200e8260f580850cd2e23d93f12
akaletekoffilevis <l.akalete20@gmail.com>
2026-08-20 17:08:19 +0100

    fix: revert rewrite, restore redirect / to /fr

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/fr",
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
